7c4a34886dac7c3be3341b805bd01e51d76f8586
Trevor Keller <trevor.keller@nist.gov>
2016-03-17 15:09:24 -0400

    add Cs, Cl iterative scheme

--- KKS.cpp
+++ KKS.cpp
@@ -10,90 +10,22 @@
 
 // Note: KKS.hpp contains important declarations and comments. Have a look.
 
+const double fA = 1.0;      // equilibrium free energy of pure A
+const double fB = 2.5;      // equilibrium free energy of pure B
+const double R = 8.314e-3;  // J/cm3-K
+const double T = 373.15;    // Isothermal.
+
+const unsigned int maxloops = 1e7;// ceiling to kill infinite loops in iterative scheme
+const double tol = 1.0e-12; // tolerance for Cs, Cl scheme to satisfy equal chemical potential
+
 namespace MMSP{
-
-double h(const double& p)     {return p;                        }
-double hprime(const double& p){return 1.0;                      }
-double g(const double& p)     {return pow(p,2.0)*pow(1.0-p,2.0);}
-double gprime(const double& p){return p*(2.0*p-1.0)*(p-1.0);    }
-
-const double fA = 1.0;    // equilibrium free energy of pure A
-const double fB = 2.5;    // equilibrium free energy of pure B
-const double R = 8.314e-3;// J/cm3-K
-const double T = 373.15;  // Isothermal.
-
-double fl(const double& c)
-{
-	// ideal solution model for liquid free energy
-	const double cA = 1.0-c;
-	const double cB = c;
-	const double tiny = 1.0e-6;
-
-	if (cA < tiny)
-		return fB;
-	else if (cB < tiny)
-		return fA;
-	return cA*fA + cB*fB + R*T*(cA*log(cA) + cB*log(cB));
-}
-
-double fs(const double& c)
-{
-	// ideal solution model for solid free energy, transformed from liquid
-	double delta = -0.25; // negative solidifies, positive melts
-	return fl(1.0-c) + delta;
-}
-
-double k()
-{
-	// Partition coefficient, from solving dfs_dc = 0 and dfl_dc = 0
-	double Cs_e = std::exp((fB-fA)/(R*T)) / (1.0 + std::exp((fB-fA)/(R*T)));
-	double Cl_e = std::exp((fA-fB)/(R*T)) / (1.0 + std::exp((fA-fB)/(R*T)));
-
-	return Cs_e / Cl_e;
-}
-
-double dfl_dc(const double& c)
-{
-	const double cA = 1.0-c;
-	const double cB = c;
-	const double tiny = 1.0e-6;
-
-	if (std::min(cA,cB) < tiny)
-		return fB-fA;
-	return fB - fA + R*T*(log(cB) - log(cA));
-}
-
-double dfs_dc(const double& c)
-{
-	return 2.0*(fA - fB) + dfl_dc(1.0-c);
-}
-
-double d2fl_dc2(const double& c)
-{
-	const double cA = 1.0-c;
-	const double cB = c;
-	return R*T/(cA*cB);
-}
-
-double d2fs_dc2(const double& c)
-{
-	return d2fl_dc2(1.0-c);
-}
-
-double f(const double& p, const double&c, const double& Cs, const double& Cl)
-{
-	const double w = 0.5; // well barrier height
-	return w*g(p) + h(p)*fs(Cs) + (1.0-h(p))*fl(Cl);
-}
-
-double d2f_dc2(const double& p, const double&c, const double& Cs, const double& Cl)
-{
-	double R = h(p)*d2fl_dc2(Cl) + (1.0-h(p))*d2fs_dc2(Cs);
-	return d2fl_dc2(Cl)*d2fs_dc2(Cs)/R;
-}
 
 void generate(int dim, const char* filename)
 {
+	int rank=0;
+	#ifdef MPI_VERSION
+	rank=MPI::COMM_WORLD.Get_rank();
+	#endif
 	/* Grid contains four fields:
 	 * 0. phi, phase fraction solid. Phi=1 means Solid.
 	 * 1. c, concentration of component A
@@ -159,11 +91,18 @@ void generate(int dim, const char* filename)
 		}
 		output(initGrid,filename);
 	} else {
-		std::cerr<<"ERROR: "<<dim<<"-dimensional domains not supported."<<std::endl
-		MMSP::Abort(-1);
+		std::cerr<<"ERROR: "<<dim<<"-dimensional domains not supported."<<std::endl;
+		exit(-1);
 	}
 	unsigned int nTot = nSol+nLiq;
-	std::cout<<"System is "<<(100*nSol)/nTot<<"% solid, "<<(100*nLiq)/nTot<<"% liquid."<<std::endl;
+	#ifdef MPI_VERSION
+	unsigned int mySol(nSol), myLiq(nLiq), myTot(nTot);
+	MPI::COMM_WORLD.Allreduce(&mySol, &nSol, 1, MPI_UNSIGNED, MPI_SUM);
+	MPI::COMM_WORLD.Allreduce(&myLiq, &nLiq, 1, MPI_UNSIGNED, MPI_SUM);
+	MPI::COMM_WORLD.Allreduce(&myTot, &nTot, 1, MPI_UNSIGNED, MPI_SUM);
+	#endif
+	if (rank==0)
+		std::cout<<"System is "<<(100*nSol)/nTot<<"% solid, "<<(100*nLiq)/nTot<<"% liquid."<<std::endl;
 
 	/* Generate Cs,Cl look-up table (LUT) using Newton-Raphson method, outlined in Provatas' Appendix C3
 	 * Store results in pureconc, which contains two fields:
@@ -173,7 +112,7 @@ void generate(int dim, const char* filename)
 	 * The grid is discretized over phi (axis 0) and c (axis 1).
 	*/
 	double C0 = (double(nLiq)*cBs + double(nSol)*cBl) / double(nSol+nLiq); // weighted average of solid + liquid
-	std::assert(C0>0.);
+	assert(C0>0.);
 	int LUTres[2] = {100, 100};
 	LUTGRID pureconc(2,0,LUTres[0],0,LUTres[1]);
 	double dp = 1.0/LUTres[0];
@@ -183,7 +122,7 @@ void generate(int dim, const char* filename)
 
 	for (int n=0; n<nodes(pureconc); n++) {
 		vector<int> x = position(pureconc,n);
-		iterateConc(dp*double(x[0]), dc*double(x[1]), pureconc[0], pureconc[1]);
+		iterateConc(dp*double(x[0]), dc*double(x[1]), pureconc(n)[0], pureconc(n)[1]);
 	}
 	output(pureconc,"consistentC.lut");
 }
@@ -205,7 +144,7 @@ template <int dim, typename T> void update(grid<dim,vector<T> >& oldGrid, int st
 		if (rank==0)
 			print_progress(step, steps);
 
-		for (int i=0; i<nodes(oldGrid); i++) {
+		for (int n=0; n<nodes(oldGrid); n++) {
 			const vector<T>& phi = oldGrid(n);
 
 			// compute laplacian
@@ -227,19 +166,108 @@ template <int dim, typename T> void update(grid<dim,vector<T> >& oldGrid, int st
 	}
 }
 
+
+} // namespace MMSP
+
+
+double fl(const double& c)
+{
+	// ideal solution model for liquid free energy
+	const double cA = 1.0-c;
+	const double cB = c;
+	const double tiny = 1.0e-6;
+
+	if (cA < tiny)
+		return fB;
+	else if (cB < tiny)
+		return fA;
+	return cA*fA + cB*fB + R*T*(cA*log(cA) + cB*log(cB));
+}
+
+double fs(const double& c)
+{
+	// ideal solution model for solid free energy, transformed from liquid
+	double delta = -0.25; // negative solidifies, positive melts
+	return fl(1.0-c) + delta;
+}
+
+double k()
+{
+	// Partition coefficient, from solving dfs_dc = 0 and dfl_dc = 0
+	double Cs_e = std::exp((fB-fA)/(R*T)) / (1.0 + std::exp((fB-fA)/(R*T)));
+	double Cl_e = std::exp((fA-fB)/(R*T)) / (1.0 + std::exp((fA-fB)/(R*T)));
+
+	return Cs_e / Cl_e;
+}
+
+double dfl_dc(const double& c)
+{
+	const double cA = 1.0-c;
+	const double cB = c;
+	const double tiny = 1.0e-6;
+
+	if (std::min(cA,cB) < tiny)
+		return fB-fA;
+	return fB - fA + R*T*(log(cB) - log(cA));
+}
+
+double dfs_dc(const double& c)
+{
+	return 2.0*(fA - fB) + dfl_dc(1.0-c);
+}
+
+double d2fl_dc2(const double& c)
+{
+	const double cA = 1.0-c;
+	const double cB = c;
+	return R*T/(cA*cB);
+}
+
+double d2fs_dc2(const double& c)
+{
+	return d2fl_dc2(1.0-c);
+}
+
+double f(const double& p, const double& c, const double& Cs, const double& Cl)
+{
+	const double w = 0.5; // well barrier height
+	return w*g(p) + h(p)*fs(Cs) + (1.0-h(p))*fl(Cl);
+}
+
+double d2f_dc2(const double& p, const double& c, const double& Cs, const double& Cl)
+{
+	double R = h(p)*d2fl_dc2(Cl) + (1.0-h(p))*d2fs_dc2(Cs);
+	return d2fl_dc2(Cl)*d2fs_dc2(Cs)/R;
+}
+
 /* Given const phase fraction (p) and concentration (c), iteratively determine
  * the solid (Cs) and liquid (Cl) fictitious concentrations that satisfy the
  * equal chemical potential constraint. Pass p and c by const value,
  * Cs and Cl by non-const reference to update in place. This allows use ofthis
  * single function to both populate the LUT and interpolate values based thereupon.
  */
-template<typename T,typename U> void iterateConc(const T p, const T c, U& Cs, U& Cl)
+template<class T> void iterateConc(const T p, const T c, T& Cs, T& Cl)
 {
-	
+	double res = 1.0; // residual for Newton-Raphson scheme
+
+	// Iterate until either the matrix is solved (residual<tolerance)
+	// or patience wears out (loop>maxloops, likely due to infinite loop).
+	unsigned int l=0;
+	while (l<maxloops && res>tol) {
+		// copy current values as "old guesses"
+		double Cso = Cs;
+		double Clo = Cl;
+		double invW = 1.0 / ( h(p)*d2fl_dc2(Clo) + (1.0-h(p))*d2fs_dc2(Cso) );
+		double f1 = h(p)*Cso + (1.0-h(p))*Clo - c;
+		double f2 = dfs_dc(Cso) - dfl_dc(Clo);
+
+		Cs = Cso + invW*(d2fl_dc2(Clo)*f1 + (1.0-h(p))*f2);
+		Cl = Clo - invW*(d2fs_dc2(Cso)*f1 - h(p)*f2);
+
+		res = std::sqrt(pow(Cs-Cso,2.0) + pow(Cl-Clo,2.0));
+		l++;
+	}
 }
-
-
-} // namespace MMSP
 
 #endif
 
--- KKS.hpp
+++ KKS.hpp
@@ -1,6 +1,7 @@
-// KKS.hpp
-// Declarations for 2D and 3D isotropic binary alloy solidification
-// Questions/comments to trevor.keller@nist.gov (Trevor Keller)
+/* KKS.hpp
+ * Declarations for 2D and 3D isotropic binary alloy solidification
+ * Questions/comments to trevor.keller@nist.gov (Trevor Keller)
+ */
 
 std::string PROGRAM = "KKS";
 std::string MESSAGE = "Isotropic phase field solidification example code";
@@ -9,10 +10,11 @@ typedef MMSP::grid<1,MMSP::vector<double> > GRID1D;
 typedef MMSP::grid<2,MMSP::vector<double> > GRID2D;
 typedef MMSP::grid<3,MMSP::vector<double> > GRID3D;
 
-// KKS requires a look-up table of Cs and Cl consistent with the constraint
-// of constant chemical potential over the full range of C and phi. This provides
-// initial guesses for iterative reconciliation of Cs and Cl with phi and c at each
-// grid point and timestep.
+/* KKS requires a look-up table of Cs and Cl consistent with the constraint
+ * of constant chemical potential over the full range of C and phi. This provides
+ * initial guesses for iterative reconciliation of Cs and Cl with phi and c at each
+ * grid point and timestep.
+ */
 typedef MMSP::grid<2,MMSP::vector<double> > LUTGRID;
 
 /* Given const phase fraction (p) and concentration (c), iteratively determine
@@ -21,5 +23,27 @@ typedef MMSP::grid<2,MMSP::vector<double> > LUTGRID;
  * Cs and Cl by non-const reference to update in place. This allows use ofthis
  * single function to both populate the LUT and interpolate values based thereupon.
  */
-template<typename T,typename U> void iterateConc(const T p, const T c, U& Cs, U& Cl);
+template<class T> void iterateConc(const T p, const T c, T& Cs, T& Cl);
 
+double h(const double& p)     {return p;                        }
+double hprime(const double& p){return 1.0;                      }
+double g(const double& p)     {return pow(p,2.0)*pow(1.0-p,2.0);}
+double gprime(const double& p){return p*(2.0*p-1.0)*(p-1.0);    }
+
+double fl(const double& c); // ideal solution model for liquid free energy density
+
+double fs(const double& c); // ideal solution model for solid free energy density
+
+double k();                 // Partition coefficient, from solving dfs_dc = 0 and dfl_dc = 0
+
+double dfl_dc(const double& c); // first derivative of fl w.r.t. c
+
+double dfs_dc(const double& c); // first derivative of fs w.r.t. c
+
+double d2fl_dc2(const double& c); // second derivative of fl w.r.t. c
+
+double d2fs_dc2(const double& c); // second derivative of fs w.r.t. c
+
+double f(const double& p, const double& c, const double& Cs, const double& Cl); // free energy density
+
+double d2f_dc2(const double& p, const double& c, const double& Cs, const double& Cl); // second derivative of f w.r.t. c
